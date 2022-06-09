980e10c83c3d82b6ad4e233804197178061908fb
Gusta-Oliviera <gustavo.ramosue@gmail.com>
2022-06-09 06:57:54 -0300

    Atualização de Arquivos

--- pythonExerciciosGu/Ex032.py
+++ pythonExerciciosGu/Ex032.py
@@ -1,5 +1,11 @@
-ano = int(input('Digite um ano: '))
-if ano % 4 == 0:
+from datetime import date
+from time import sleep
+ano = int(input('Digite um ano para análise: '))
+print('Análisando...')
+sleep(3)
+if ano == 0:
+    ano = date.today().year
+if ano % 4 == 0 and ano % 100 != 0 and ano % 400 == 0:
     print(f'O ano {ano} é bissexto!')
 else:
     print(f'O ano {ano} não é bissexto!')
