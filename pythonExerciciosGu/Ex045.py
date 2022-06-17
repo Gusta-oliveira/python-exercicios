0cd5ad810380387433d941db8b20b8c32803cc3e
Gusta-Oliviera <gustavo.ramosue@gmail.com>
2022-06-17 02:43:07 -0300

    Att arquivos

--- pythonExerciciosGu/Ex045.py
+++ pythonExerciciosGu/Ex045.py
@@ -1,45 +1,69 @@
 from random import randint
 from time import sleep
-escolha = int(input("""Digite sua escolha:
+jogador = int(input("""Digite sua escolha:
 [0]Pedra
 [1]Papel
 [2]Tesoura
 """))
 itens = ('pedra', 'papel', 'tesoura')
-esc = randint(0, 2)
+computador = randint(0, 2)
 
-sleep(1)
 print('JO')
 sleep(1)
 print('KEN')
 sleep(1)
 print('PO!!!')
-sleep(1)
 
 print('{:=^25}'.format('JO-KEN-PO'))
-print('Computador jogou {}'.format(itens[esc]))
-print('Jogador jogou {}'.format(itens[escolha]))
 
-# Testando condição de escolha ser Pedra
-if escolha == 0 and escolha == esc:
-    print('EMPATE!')
-elif escolha != esc and escolha == 0 and esc == 1:
-    print('Computador GANHOU!')
-elif escolha != esc and escolha == 0 and esc == 2:
-    print('Jogador GANHOU!')
+# Testando condição de computador ser Pedra
+if computador == 0:
+    if jogador == 0:
+        print('Computador jogou {}'.format(itens[computador]))
+        print('Jogador jogou {}'.format(itens[jogador]))
+        print('EMPATE!')
+    elif jogador == 1:
+        print('Computador jogou {}'.format(itens[computador]))
+        print('Jogador jogou {}'.format(itens[jogador]))
+        print('Jogador GANHOU')
+    elif jogador == 2:
+        print('Computador jogou {}'.format(itens[computador]))
+        print('Jogador jogou {}'.format(itens[jogador]))
+        print('Computador GANHOU!')
+    else:
+        print('Jogada inválida!')
+# Testando condição de computador ser Papel
+if computador == 1:
+    if jogador == 0:
+        print('Computador jogou {}'.format(itens[computador]))
+        print('Jogador jogou {}'.format(itens[jogador]))
+        print('Computador GANHOU!')
+    elif jogador == 1:
+        print('Computador jogou {}'.format(itens[computador]))
+        print('Jogador jogou {}'.format(itens[jogador]))
+        print('EMPATE!')
+    elif jogador == 2:
+        print('Computador jogou {}'.format(itens[computador]))
+        print('Jogador jogou {}'.format(itens[jogador]))
+        print('Jogador GANHOU!')
+    else:
+        print('Jogada inválida!')
 
-# Testando condição de escolha ser Papel
-if escolha == 1 and escolha == esc:
-    print('EMPATE!')
-elif escolha != esc and escolha == 1 and esc == 0:
-    print('Jogador GANHOU!')
-elif escolha != esc and escolha == 1 and esc == 2:
-    print('Computador GANHOU!')
-# Testando condição de escolha ser Tesoura2
-if escolha == 2 and escolha == esc:
-    print('EMPATE!')
-elif escolha != esc and escolha == 2 and esc == 1:
-    print('Jogador GANHOU!')
-elif escolha != esc and escolha == 2 and esc == 0:
-    print('Computador GANHOU!')
+# Testando condição de computador ser Tesoura
+if computador == 2:
+    if jogador == 0:
+        print('Computador jogou {}'.format(itens[computador]))
+        print('Jogador jogou {}'.format(itens[jogador]))
+        print('Jogador GANHOU!')
+    elif jogador == 1:
+        print('Computador jogou {}'.format(itens[computador]))
+        print('Jogador jogou {}'.format(itens[jogador]))
+        print('Computador GANHOU!')
+    elif jogador == 2:
+        print('Computador jogou {}'.format(itens[computador]))
+        print('Jogador jogou {}'.format(itens[jogador]))
+        print('EMPATE!')
+    else:
+        print('Jogada inválida!')
 print('=' * 25)
+
